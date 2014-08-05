ada784d98769083249efd3ec9fb716bf13e2c945
Kim Hoffman <khoffman@pivotallabs.com>
2014-08-05 14:22:01 -0700

    Adding redirect for deleted topic

--- redirects.rb
+++ redirects.rb
@@ -1,2 +1,3 @@
 r302 '/bosh/bosh-errands.html', '/bosh/jobs.html'
 r302 '/deploying/vsphere/cloud-foundry-example-manifest.html', '/deploying/vsphere/index.html'
+r302 '/deploying/vcloud/cloud-foundry-example-manifest.html', '/deploying/vcloud/index.html'
