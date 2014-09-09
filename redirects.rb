cb19751e1002e2472b7a596994ebd4f744b0dc0e
Max Hufnagel <mhufnagel@pivotallabs.com>
2014-09-09 10:09:03 -0700

    add redirect from api-endpoint to cf-api-endpoint

--- redirects.rb
+++ redirects.rb
@@ -1,3 +1,4 @@
 r302 '/bosh/bosh-errands.html', '/bosh/jobs.html'
 r302 '/deploying/vsphere/cloud-foundry-example-manifest.html', '/deploying/vsphere/index.html'
 r302 '/deploying/vcloud/cloud-foundry-example-manifest.html', '/deploying/vcloud/index.html'
+r302 '/devguide/deploy-apps/api-endpoint.html', '/running/cf-api-endpoint.html'
