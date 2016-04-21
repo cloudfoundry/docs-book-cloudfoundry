95193c2855f513a873e14ba8d7978afe12823a97
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-04-21 13:22:44 +0200

    rewrite git to https

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -o StrictHostKeyChecking=no git@github.com
+git config --global url."https://github.com".insteadOf git://github.com
 
 bundle install --binstubs
 bundle exec bookbinder bind remote
