5136260291244474a11ee9527d7029a2521dfb2e
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-04-21 15:05:21 +0200

    use correct github url

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ssh -o StrictHostKeyChecking=no git@github.com
-git config --global url."https://github.com".insteadOf git://github.com
+git config --global url."https://github.com/".insteadOf git@github.com:
 
 bundle install --binstubs
 bundle exec bookbinder bind remote
