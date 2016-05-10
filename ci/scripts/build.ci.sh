bd0d5c7bafc5c0bb4bbbdb4ea60d16aaa30b182e
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-05-10 16:39:57 +0200

    fix linting issues

--- ci/scripts/build.ci.sh
+++ ci/scripts/build.ci.sh
@@ -5,7 +5,7 @@ git config --global url."https://github.com/".insteadOf git@github.com:
 
 ci/scripts/build.sh
 
-export DOCS_VERSION = $(cat ../docs-version/version)
+export DOCS_VERSION=$(cat ../docs-version/version)
 
-tar -cvzf docs-$DOCS_VERSION.tgz final_app/public
-mv docs-$DOCS_VERSION.tgz ../final-app/
+tar -cvzf "docs-$DOCS_VERSION.tgz" final_app/public
+mv "docs-$DOCS_VERSION.tgz" ../final-app/
