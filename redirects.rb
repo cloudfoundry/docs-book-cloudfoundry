0c79fe9a8c55294d126df4369a70091d64e90bf3
Melinda Gutermuth <mjgutermuth@gmail.com>
2017-02-27 11:35:46 -0800

    Fixes redirect for CF networking [#137755051]

--- redirects.rb
+++ redirects.rb
@@ -66,6 +66,6 @@ r301 '/services/volume-services-v2.9.html', '/devguide/services/using-vol-servic
 r301 '/services/volume-services.html', '/devguide/services/using-vol-services.html'
 r301 %r{/services/api-v(.*)}, '/services/api.html'
 
-r301 'adminguide/container-networking.html', '/devguide/deploy-apps/cf-networking.html'
+r301 '/adminguide/container-networking.html', '/devguide/deploy-apps/cf-networking.html'
 
 r301 '/adminguide/deploy-vol-services.html', '/running/deploy-vol-services.html'
