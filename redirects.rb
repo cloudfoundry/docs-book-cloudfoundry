f6b62cea1c54929cad917e219aee7b8e7793a9a7
Melinda Gutermuth <mjgutermuth@gmail.com>
2017-05-31 11:39:34 -0700

    Removes subnavs for pre-Diego troubleshooting topics and adds redirects [#145867931]

--- redirects.rb
+++ redirects.rb
@@ -43,6 +43,8 @@ r301 '/running/managing-cf/', '/running/index.html#managing'
 r301 '/running/managing-cf/index.html', '/running/index.html#managing'
 r301 '/running/troubleshooting/', '/running/index.html#troubleshooting'
 r301 '/running/troubleshooting/index.html', '/running/index.html#troubleshooting'
+r301 '/running/troubleshooting/troubleshooting-apps.html', '/running/index.html#troubleshooting'
+r301 '/running/troubleshooting/troubleshooting-warden-services', '/running/index.html#troubleshooting'
 r301 '/running/using-vol-services.html', '/devguide/services/using-vol-services.html'
 r301 '/running/api/version.html', '/running/cf-api-endpoint.html'
 
