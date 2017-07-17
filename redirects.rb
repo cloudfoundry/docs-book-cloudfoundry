a19da05df1eb1a25597d325e2e7fb40660282bc0
Ben Tarnoff <btarnoff@pivotal.io>
2017-07-17 14:22:49 -0700

    remove osbapi and add redirects

--- redirects.rb
+++ redirects.rb
@@ -91,3 +91,6 @@ r301 '/dea-vs-diego.html', '/diego-architecture.html'
 r301 '/running/troubleshooting/error-codes.html', '/running/index.html'
 r301 '/running/troubleshooting/v2-errors.html', '/running/index.html'
 r301 '/running/troubleshooting/v1-errors.html', '/running/index.html'
+
+r301 '/services/api.html', 'https://github.com/openservicebrokerapi/servicebroker/blob/v2.12/spec.md'
+r301 '/services/release-notes.html', 'https://github.com/openservicebrokerapi/servicebroker/blob/v2.12/release-notes.md'
