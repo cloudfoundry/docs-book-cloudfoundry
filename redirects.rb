1804e765835845bd7ad5e5799a5541ff8e486986
Ben Tarnoff <btarnoff@pivotal.io>
2017-09-15 14:37:01 -0400

    add redirects for OSBAPI topics[#151128504]

--- redirects.rb
+++ redirects.rb
@@ -97,3 +97,7 @@ r301 '/services/release-notes.html', 'https://github.com/openservicebrokerapi/se
 
 r301 '/adminguide/uaa-user-management.html', '/uaa/uaa-user-management.html'
 r301 '/buildpacks/detection.html', '/buildpacks/understand-buildpacks.html'
+
+#OSBAPI Redirects
+r301 '/services/catalog-metadata.html', 'https://github.com/openservicebrokerapi/servicebroker/blob/master/profile.md#service-metadata'
+r301 '/services/volume-services.html', 'https://github.com/openservicebrokerapi/servicebroker/blob/master/spec.md#volume-mounts-object'
