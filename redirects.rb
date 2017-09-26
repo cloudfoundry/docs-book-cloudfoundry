ffc5fbae5d7e9c19131c5103d31c3e9c9cbcc65d
animatedmax <mhufnagel@pivotal.io>
2017-09-26 15:42:33 -0700

    http to https redirect

--- redirects.rb
+++ redirects.rb
@@ -1,4 +1,4 @@
-# r301 %r{/(.*)}, 'https://docs.cloudfoundry.org/$1'
+r301 %r{http://docs.cloudfoundry.org/(.*)}, 'https://docs.cloudfoundry.org/$1'
 r301 %r{/bosh/(.*)}, 'http://bosh.io/docs/$1'
 r301 '/console/cf-api-endpoint.html', '/running/cf-api-endpoint.html'
 
