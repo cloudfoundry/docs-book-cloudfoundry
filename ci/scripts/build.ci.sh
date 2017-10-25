876ccbdfc6b76e3ba9e9956cb84fb72b745d319c
Tobias Fuhrimann <tobias.fuhrimann@gmail.com>
2017-10-25 15:00:43 +0200

    Add manifest files to tarball

--- ci/scripts/build.ci.sh
+++ ci/scripts/build.ci.sh
@@ -6,5 +6,5 @@ ci/scripts/build.sh
 
 version=$(cat ../docs-version/version)
 
-tar -cvzf "docs-${version}.tgz" final_app/public
+tar -cvzf "docs-${version}.tgz" final_app/public manifest.yml manifest.dev.yml
 mv "docs-${version}.tgz" ../final-app/
